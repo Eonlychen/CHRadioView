d3cdd34c59f0167157b9a93adc91f9812d2f1160
Eonlychen <eonly_chen@163.com>
2019-04-02 11:10:42 +0800

    add test code

--- CHRadioView/ViewController.m
+++ CHRadioView/ViewController.m
@@ -21,8 +21,22 @@
 
     CHRadioView *radioView = [CHRadioView showRadioViewWithTitles: @[@"第一奖杯",@"第二奖杯",@"第三奖杯"] imagesNormal: @[@"cup-first",@"cup-second",@"cup-third"] imagesSelected:nil frame:CGRectMake(0, 200, CGRectGetWidth(self.view.frame), 80)];
     
+    radioView.backgroundColorNormal = UIColor.yellowColor;
+    radioView.backgroundColorSelected = UIColor.redColor;
+    radioView.titleFontNormal = 12;
+    radioView.titleFontSelected = 16;
+    radioView.titleColorNormal = UIColor.blueColor;
+    radioView.titleColorSelected = UIColor.greenColor;
+    
+//    // 重新设置itemsize的代码，如果没有 不要调用set方法
+//    [radioView setItemSizeBlock:^CGSize(UICollectionViewLayout * _Nonnull layout, NSIndexPath * _Nonnull indexPath) {
+//        return CGSizeMake(100, 60);
+//    }];
+    [radioView setSelectedItemBlock:^(CCRadioModel * _Nonnull model, NSIndexPath * _Nonnull indexPath) {
+        
+    }];
+    
     [self.view addSubview:radioView];
-
 
 }
 
